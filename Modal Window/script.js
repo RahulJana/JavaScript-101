ba61ac50ccbe63a8800628e8ef847ae7e971d3a5
Rahul Jana <i.rahuljana@gmail.com>
2022-12-29 20:54:39 +0530

    Setting-up buttons for modal window.

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,2 +1,12 @@
 "use strict";
- 
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-model");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+console.log(btnsOpenModal);
+
+for(let i = 0; i<btnsOpenModal.length; i++){
+    console.log(btnsOpenModal[i].textContent);
+}
