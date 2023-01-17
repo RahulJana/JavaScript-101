e9fcab2155d5a69c135ed4ed178ab8eac4bff0ea
Rahul Jana <i.rahuljana@gmail.com>
2023-01-17 22:36:04 +0530

    Implemented modal window with opening and closing of clicks.

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -2,11 +2,28 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModel = document.querySelector(".close-model");
+const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 console.log(btnsOpenModal);
 
-for(let i = 0; i<btnsOpenModal.length; i++){
-    console.log(btnsOpenModal[i].textContent);
+const openModal = function () {
+    console.log("Button clicked!!");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", openModal);
 }
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+/*
+Here we did not used closeModal() <-[With parenthesis], because this would have called the the function as soon as JS engine compile the code. But we want to execute the function when it encounters the the click via EventListener().
+*/
