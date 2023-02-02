f87894020e9cd83978833ee25a5b6557355805af
Rahul Jana <i.rahuljana@gmail.com>
2023-02-03 00:06:09 +0530

    Using space as spaceholder and swaping using destructuring.

---
+++ Advance JS features/Destructuring arrays and objects/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// Data needed for a later exercise
+const flights =
+    "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// Data needed for first part of the section
+const restaurant = {
+    name: "Classico Italiano",
+    location: "Via Angelo Tavanti 23, Firenze, Italy",
+    categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+    starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+    mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
+};
+
+const arr = [1, 2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+console.log(a, b, c);
+
+const [a1, a2, a3, a4] = arr;
+console.log(a1, a2, a3, a4);
+console.log(arr);
+
+let [first, second] = restaurant.categories;
+console.log(first, second);
+
+// Using space as a space holder.
+let [item_1, , item_3] = restaurant.categories;
+console.log(item_1, item_3);
+
+// Swapping without destructuring.
+/*
+let temp = item_1;
+item_1 = item_2;
+item_2 = temp;
+*/
+
+// With destructuring
+[item_1, item_3] = [item_3, item_1];  // Like Python
+console.log(item_1, item_3);
